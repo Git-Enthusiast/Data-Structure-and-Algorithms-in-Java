6ef92c8fa7d54669ce1908b6f6d0a71d347190e8
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2023-12-18 23:51:29 +0530

    Lecture 1

---
+++ src/com/Lec1/Basics.java
@@ -0,0 +1,19 @@
+package com.Lec1;
+
+public class Basics {
+    public static void main(String[] args) {
+        int a = 10;
+         if (a == 10){
+             System.out.println( " Hello world!");
+
+         }
+         int count = 1;
+         while (count != 20){
+             System.out.println(count);
+             count++;
+         }
+         for (int i = 1 ; i<=15;i++ ){
+             System.out.println(i);
+         }
+    }
+}
