feb4b48f8cd94f25b4399279566dbfe6a840d741
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2023-12-18 23:52:26 +0530

    Lecture 1

---
+++ src/com/Lec1/PrimitivesDatatype.java
@@ -0,0 +1,13 @@
+package com.Lec1;
+
+public class PrimitivesDatatype {
+    public static void main(String[] args) {
+    int number = 91;
+    float marks = 1091.5f;
+    char letters = 'I';
+    double LargeDecimalNumber = 1091.53384304324324;
+    long LargeInteger = 109129062003L;
+    boolean check = true;
+
+    }
+}
