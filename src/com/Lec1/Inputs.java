75adb8cd1eacfd60cfd2c9dff1472884b8806c99
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2023-12-18 23:51:52 +0530

    Lecture 1

---
+++ src/com/Lec1/Inputs.java
@@ -0,0 +1,16 @@
+package com.Lec1;
+
+import java.util.Scanner;
+
+public class Inputs {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+//        System.out.println("Enter your Roll no :" );
+//        int Roll = input.nextInt();
+//        System.out.println("Your Roll no is:"+ Roll);
+//        int a = 234_000_000;
+//        System.out.println(a);
+        String name = input.nextLine();
+        System.out.println(name);
+    }
+}
