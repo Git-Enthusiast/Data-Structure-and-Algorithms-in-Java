594e1e4e5923a95c721152b88110435c518fe618
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2023-12-19 22:03:14 +0530

    Lecture 2

---
+++ src/com/Lec2/Fibonacci.java
@@ -0,0 +1,23 @@
+package com.Lec2;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the value of n :");
+        int n = in.nextInt();
+        int p = 0;
+        int c = 1;
+        int count = 2;
+        while (count<=n){
+            int temp = c;
+            c = c+p;
+            p = temp;
+            count ++;
+
+        }
+        System.out.println("Your "+n+"th"+" Fibonacci number is : "+c);
+
+    }
+}
