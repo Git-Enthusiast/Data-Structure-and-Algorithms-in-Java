1efe70f1ca5f0cd4a45710d51117a7d80c245be7
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2023-12-21 13:13:07 +0530

    Lecture 2

---
+++ src/com/Lec4/PassingExample.java
@@ -0,0 +1,15 @@
+package com.Lec4;
+
+
+
+public class PassingExample {
+    public static void main(String[] args) {
+        String superhero = "Iron Man";
+        greet(superhero);
+    }
+
+    private static void greet(String naam) {
+
+        System.out.println(naam);
+    }
+}
