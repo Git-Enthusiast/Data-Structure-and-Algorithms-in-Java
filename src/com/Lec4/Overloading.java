14ca46696f56a5961a3bd3856ca3435610eef134
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2023-12-24 16:27:18 +0530

    Lecture 2

---
+++ src/com/Lec4/Overloading.java
@@ -0,0 +1,28 @@
+package com.Lec4;
+
+public class Overloading {
+    public static void main(String[] args) {
+//        fun(67);
+//        fun("Rajan Raj");
+        int ans = sum(3, 4, 78);
+        System.out.println(ans);
+    }
+
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+    static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    static void fun(int a) {
+        System.out.println("first one");
+        System.out.println(a);
+    }
+
+    static void fun(String name) {
+        System.out.println("Second one");
+        System.out.println(name);
+    }
+}
