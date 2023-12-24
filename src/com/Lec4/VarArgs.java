fdab83c9a0f0aa1d0e42a234dd2fce9549c5207b
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2023-12-24 16:26:15 +0530

    Lecture 2

---
+++ src/com/Lec4/VarArgs.java
@@ -0,0 +1,29 @@
+package com.Lec4;
+
+import java.util.Arrays;
+
+public class VarArgs {
+    public static void main(String[] args) {
+        fun();// variable arguments may be null
+        multiple(2, 3, "Rajan", "Rahul", "om");
+        demo(23,58,23,54,56,7); // calling function of line 12
+        demo("Rajan","Raj","Om","Vishnu Majumdar"); // calling function of line 17
+    }
+
+    static void demo(int ...v) {
+        System.out.println(Arrays.toString(v));
+    }
+
+    static void demo(String ...v) {
+        System.out.println(Arrays.toString(v));
+    }
+
+    static void multiple(int a, int b, String ...v) {
+        System.out.println(Arrays.toString(v));
+
+    }
+
+    static void fun(int ...v) {
+        System.out.println(Arrays.toString(v));
+    }
+}
