79e433873edf8223e19ab927a234b242dc44548d
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2023-12-31 02:09:19 +0530

    Pattern.Practice

---
+++ src/com/Practice/Pattern/PatternWithoutArrayStringCollection.java
@@ -0,0 +1,42 @@
+package com.Practice.Pattern;
+
+public class PatternWithoutArrayStringCollection {
+    public static void main(String[] args) {
+        int n = 5; // Change the value of n to adjust the size of the pattern
+
+        // Upper part of the pattern
+        for (int i = 1; i <= n; i++) {
+
+            // Inner loop for spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+
+            // Inner loop for printing pattern
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+
+            // Move to the next line after each row is printed
+            System.out.println();
+        }
+
+        // Lower part of the pattern
+        for (int i = n - 1; i >= 1; i--) {
+
+            // Inner loop for spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+
+            // Inner loop for printing pattern
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+
+            // Move to the next line after each row is printed
+            System.out.println();
+        }
+    }
+}
+
