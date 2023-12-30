bf307a7f9e6fe4f0ea3dd85bfaa304d56b5bbd48
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2023-12-31 02:06:20 +0530

    HackerRank

---
+++ src/com/Practice/HackerRank/Solution.java
@@ -0,0 +1,19 @@
+package com.Practice.HackerRank;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        String s = scan.nextLine();
+        double d= scan.nextDouble();
+        scan.close();
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
