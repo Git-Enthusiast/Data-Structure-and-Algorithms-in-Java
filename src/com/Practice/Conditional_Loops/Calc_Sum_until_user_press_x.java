2161466e31b1941031a300e4a9e697cf8f629ed1
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2024-02-02 22:44:06 +0530

    Conditional_Loops

--- src/com/Practice/Conditional_Loops/Calc_Sum_until_user_press_x.java
+++ src/com/Practice/Conditional_Loops/Calc_Sum_until_user_press_x.java
@@ -14,6 +14,6 @@ public class Calc_Sum_until_user_press_x {
             System.out.println("Please Enter 'x' to Stop or Other to Continue:");
             ch = input.next().trim().charAt(0);
         } while (ch !='x');
-        System.out.println(sum);
+        System.out.println("sum = "+sum);
     }
 }
