71a5efd34815bc230681e92ba55604afa9763c5e
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2024-02-02 22:22:05 +0530

    Conditional_loops

---
+++ src/com/Practice/Conditional_Loops/Calc_Sum_until_user_press_x.java
@@ -0,0 +1,19 @@
+package com.Practice.Conditional_Loops;
+
+import java.util.Scanner;
+
+public class Calc_Sum_until_user_press_x {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+        char ch;
+        do {
+            System.out.println("Please Enter value of Integer:");
+            int num = input.nextInt();
+            sum = sum +num;
+            System.out.println("Please Enter 'x' to Stop or Other to Continue:");
+            ch = input.next().trim().charAt(0);
+        } while (ch !='x');
+        System.out.println(sum);
+    }
+}
