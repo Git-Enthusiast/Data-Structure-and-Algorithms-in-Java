f721f49bc9ee21933f48ee472ef3fa0b5ea006c9
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2024-02-08 18:31:58 +0530

    Conditional_Loops

--- src/com/Practice/Conditional_Loops/AreaOfTriangle.java
+++ src/com/Practice/Conditional_Loops/AreaOfTriangle.java
@@ -8,15 +8,48 @@ Area of Triangle.
 public class AreaOfTriangle {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.println("Please Enter '1' If you want to find the area of Triangle with Base and Height, ");
+        System.out.println("Or Please Enter '0' If you want to find the area of Triangle with Three sides of the triangle. ");
+        int check = input.nextInt();
+        if (check == 0) {
+            calcAreaWithSidesOfTriangle();
+        } else if (check == 1) {
+            calcAreaWithBaseAndHeight();
+        } else {
+            System.out.println("Invalid Input, Please Run the Program again.");
+        }
+    }
+
+    static void calcAreaWithBaseAndHeight() {
+        Scanner input = new Scanner(System.in);
         System.out.println("Please Enter the value of Base and Height respectively:");
         System.out.print("Base = ");
         double base = input.nextDouble();
         System.out.print("Height = ");
         double height = input.nextDouble();
-        calcArea(base,height);
+        double area = (base * height) / 2;
+        System.out.println("The area of Triangle whose base " + base + " and height " + height + " is equal to: " + area + " unit².");
     }
-    static void calcArea(double base,double height){
-        double area = (base*height)/2;
-        System.out.println("The area of Triangle whose base "+(int)base+" and height "+(int)height+" is equal to: "+area+" unit².");
+
+    static void calcAreaWithSidesOfTriangle() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please Enter the value of Three sides respectively: ");
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+
+        // Check if the sides form a valid triangle
+        if (isValidTriangle(a, b, c)) {
+            double s = (a + b + c) / 2;
+            double area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+            System.out.println("The area of the Triangle whose sides are " + a + ", " + b + ", " + c + ", is equal to : " + area + " unit².");
+        } else {
+            System.out.println("Invalid input. The given sides do not form a valid triangle.");
+        }
+    }
+
+    static boolean isValidTriangle(double a, double b, double c) {
+        return a + b > c && a + c > b && b + c > a;
     }
 }
+
