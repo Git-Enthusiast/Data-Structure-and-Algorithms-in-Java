9b58a2316d94d34355546a022bdf87b9d5398e68
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2024-02-17 12:25:05 +0530

    Lec4

--- src/com/Lec4/Overloading.java
+++ src/com/Lec4/Overloading.java
@@ -1,11 +1,22 @@
 package com.Lec4;
 
+import java.util.Scanner;
+
 public class Overloading {
     public static void main(String[] args) {
 //        fun(67);
 //        fun("Rajan Raj");
         int ans = sum(3, 4, 78);
         System.out.println(ans);
+        sum();
+    }
+
+    private static void sum() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please Enter the value of num1 and num2 for addition: ");
+        int num1 = input.nextInt();
+        int num2 = input.nextInt();
+        System.out.println("Addition = "+(num1+num2));
     }
 
     static int sum(int a, int b) {
