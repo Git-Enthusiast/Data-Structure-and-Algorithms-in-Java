1c39cb1bc773ed098f540d01848ff398519b1274
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2024-02-20 19:58:19 +0530

    Lec4

--- src/com/Lec4/Scope.java
+++ src/com/Lec4/Scope.java
@@ -4,7 +4,7 @@ public class Scope {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        String name = "Kunal";
+        String name ="kunal";
         {
 //          int a = 78; // already initialised outside the block in the same method, hence you cannot initialise again
             a = 100; // reassign the origin ref variable to some other value
