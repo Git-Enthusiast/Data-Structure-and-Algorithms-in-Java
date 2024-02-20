d4f170eb42148db43cf20a5736159570962df8ad
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2024-02-20 20:13:50 +0530

    Java Ct -2

---
+++ src/com/JavaExam/Question_2.java
@@ -0,0 +1,11 @@
+package com.JavaExam;
+
+class Question_2
+{
+    public static void main(String args[])
+    {
+        int arr[] = {1, 2, 3, 4, 5};
+        for ( int i = 0; i < arr.length - 2; ++i)
+            System.out.println(arr[i] + " ");
+    }
+}
