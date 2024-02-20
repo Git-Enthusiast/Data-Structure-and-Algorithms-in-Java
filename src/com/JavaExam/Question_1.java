a17090b259ec1178337d9251b64410c629602a59
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2024-02-20 19:58:53 +0530

    Java Ct -2

---
+++ src/com/JavaExam/Question_1.java
@@ -0,0 +1,13 @@
+package com.JavaExam;
+
+public class Question_1 {
+    public static void calculate() {
+        int ans = 42 + 45 - 48 - 5 - 15 + 20 * 2;
+        System.out.println("Value of ans = " + ans);
+    }
+
+    public static void main(String[] args) {
+        calculate();
+    }
+}
+
