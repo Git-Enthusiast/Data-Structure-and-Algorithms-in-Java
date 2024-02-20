01b7e5fb7f624a46848da51eb701b0581468428a
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2024-02-20 20:17:34 +0530

    Java Ct -2

---
+++ src/com/JavaExam/String_demo.java
@@ -0,0 +1,11 @@
+package com.JavaExam;
+
+class String_demo
+{
+    public static void main(String[] args)
+    {
+        char[] chars = {'a', 'b', 'c'};
+        String s = new String(chars);
+        System.out.println(s);
+    }
+}
