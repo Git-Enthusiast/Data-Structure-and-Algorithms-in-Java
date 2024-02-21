79fd0ed98d0919c5890d28fc2b312aecad601b1c
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2024-02-21 09:41:42 +0530

    Java Ct -2

---
+++ src/com/JavaExam/FinalVariableExample.java
@@ -0,0 +1,11 @@
+package com.JavaExam;
+
+public class FinalVariableExample {
+    final int MAX_VALUE = 100;
+
+    public static void main(String[] args) {
+        FinalVariableExample example = new FinalVariableExample();
+        System.out.println("Maximum value: " + example.MAX_VALUE);
+    }
+}
+
