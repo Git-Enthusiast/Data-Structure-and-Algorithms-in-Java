4ad1f4d283993f75e04c634fe948ee9e8d724705
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2024-02-21 08:36:50 +0530

    Java Ct -2

---
+++ src/com/JavaExam/StaticMembers/ConstantsExample.java
@@ -0,0 +1,11 @@
+package com.JavaExam.StaticMembers;
+
+public class ConstantsExample {
+    // Static final variable (constant)
+    static final int MAX_VALUE = 100;
+
+    public static void main(String[] args) {
+        // Accessing a static final variable
+        System.out.println("Maximum value: " + ConstantsExample.MAX_VALUE);
+    }
+}
