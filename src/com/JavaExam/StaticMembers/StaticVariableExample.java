50988fdfdace612c4daa354981d54fa5ad9f1af0
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2024-02-21 08:29:48 +0530

    Java Ct -2

---
+++ src/com/JavaExam/StaticMembers/StaticVariableExample.java
@@ -0,0 +1,13 @@
+package com.JavaExam.StaticMembers;
+
+public class StaticVariableExample {
+    // Static variable
+    static int staticVar = 10;
+
+    public static void main(String[] args) {
+        // Accessing a static variable
+        System.out.println("Static variable value: " + StaticVariableExample.staticVar);
+        System.out.println(staticVar);
+    }
+}
+
