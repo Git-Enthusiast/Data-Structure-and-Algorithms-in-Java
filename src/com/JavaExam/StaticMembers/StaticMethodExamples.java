fbf4d461174fba84abdfd89388751f7b6cd470ba
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2024-02-21 08:54:33 +0530

    Java Ct -2

---
+++ src/com/JavaExam/StaticMembers/StaticMethodExamples.java
@@ -0,0 +1,14 @@
+package com.JavaExam.StaticMembers;
+
+public class StaticMethodExamples {
+    // Static method
+    static void staticMethod() {
+        System.out.println("This is a static method");
+    }
+
+    public static void main(String[] args) {
+        // Calling a static method using the class name
+        StaticMethodExamples.staticMethod();
+    }
+}
+
