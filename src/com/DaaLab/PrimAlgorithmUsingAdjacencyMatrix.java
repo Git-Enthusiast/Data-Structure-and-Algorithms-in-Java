25760defd2bf15c5fa0d915f7d10e5c9f86de3bd
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2024-02-27 20:48:20 +0530

    Daa Lab

--- src/com/DaaLab/PrimAlgorithmUsingAdjacencyMatrix.java
+++ src/com/DaaLab/PrimAlgorithmUsingAdjacencyMatrix.java
@@ -1,5 +1,29 @@
 package com.DaaLab;
 
+/*
+Enter the number of vertices: 5
+Please Enter 'n*n' matrix the same is given below for vertices = 5 (for sample):
+0 2 0 6 0
+2 0 3 8 5
+0 3 0 0 7
+6 8 0 0 9
+0 5 7 9 0
+
+Enter the adjacency matrix:
+0 2 0 6 0
+2 0 3 8 5
+0 3 0 0 7
+6 8 0 0 9
+0 5 7 9 0
+Edge 	Weight
+0 - 1	2
+1 - 2	3
+0 - 3	6
+1 - 4	5
+Total weight of MST: 16
+Execution time for Adjacency Matrix Representation: 26 milliseconds
+
+ */
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -7,7 +31,7 @@ public class PrimAlgorithmUsingAdjacencyMatrix {
 
     private static final int INF = Integer.MAX_VALUE;
 
-    public void primMST(int graph[][]) {
+    public int primMST(int graph[][]) {
         int V = graph.length;
         int[] parent = new int[V];
         int[] key = new int[V];
@@ -18,6 +42,7 @@ public class PrimAlgorithmUsingAdjacencyMatrix {
 
         key[0] = 0;
         parent[0] = -1;
+        int totalWeight = 0;
 
         for (int count = 0; count < V - 1; count++) {
             int u = minKey(key, mstSet);
@@ -30,7 +55,12 @@ public class PrimAlgorithmUsingAdjacencyMatrix {
                 }
         }
 
+        for (int i = 1; i < V; i++) {
+            totalWeight += graph[i][parent[i]];
+        }
+
         printMST(parent, graph);
+        return totalWeight;
     }
 
     private int minKey(int[] key, boolean[] mstSet) {
@@ -74,11 +104,13 @@ public class PrimAlgorithmUsingAdjacencyMatrix {
         PrimAlgorithmUsingAdjacencyMatrix prim = new PrimAlgorithmUsingAdjacencyMatrix();
 
         long startTime = System.currentTimeMillis();
-        prim.primMST(graph);
+        int totalWeight = prim.primMST(graph);
         long endTime = System.currentTimeMillis();
+        System.out.println("Total weight of MST: " + totalWeight);
         System.out.println("Execution time for Adjacency Matrix Representation: " + (endTime - startTime) + " milliseconds");
 
         scanner.close();
     }
 }
+
 
