e5e875cea4a588fc18fd2c51dee21501c7d8c9fa
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2024-02-27 11:29:08 +0530

    DaaLab

---
+++ src/com/DaaLab/ZeroOneKnapsack.java
@@ -0,0 +1,58 @@
+package com.DaaLab;
+
+import java.util.Scanner;
+
+public class ZeroOneKnapsack {
+
+    public static int knapsack(int[] weights, int[] values, int capacity) {
+        int n = weights.length;
+        int[][] dp = new int[n + 1][capacity + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int w = 0; w <= capacity; w++) {
+                if (i == 0 || w == 0)
+                    dp[i][w] = 0;
+                else if (weights[i - 1] <= w)
+                    dp[i][w] = Math.max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w]);
+                else
+                    dp[i][w] = dp[i - 1][w];
+            }
+        }
+
+        return dp[n][capacity];
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of items: ");
+        int n = scanner.nextInt();
+
+        int[] weights = new int[n];
+        int[] values = new int[n];
+
+        System.out.println("Enter the weights of the items:");
+        for (int i = 0; i < n; i++) {
+            weights[i] = scanner.nextInt();
+        }
+
+        System.out.println("Enter the values of the items:");
+        for (int i = 0; i < n; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        System.out.print("Enter the capacity of the knapsack: ");
+        int capacity = scanner.nextInt();
+
+        long startTime = System.nanoTime();
+        int maxVal = knapsack(weights, values, capacity);
+        long endTime = System.nanoTime();
+        long duration = (endTime - startTime);
+
+        System.out.println("Maximum value that can be obtained: " + maxVal);
+        System.out.println("Execution time: " + duration + " nanoseconds");
+
+        scanner.close();
+    }
+}
+
