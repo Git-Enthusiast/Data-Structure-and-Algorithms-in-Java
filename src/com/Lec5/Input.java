5a0a058e33a958a4fceff6b1bbbebd28a00d8bbc
Git-Enthusiast <messachuate@gmail.com>
2024-03-06 00:44:27 +0530

    Arrays Updates

--- src/com/Lec5/Input.java
+++ src/com/Lec5/Input.java
@@ -7,6 +7,9 @@ public class Input {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int[] arr = new  int[5];
+        for (int i = 0 ; i< arr.length;i++){
+           arr[i]= in.nextInt();
+        }
         arr[0] =24;
         arr[1] =25;
         arr[2] =26;
@@ -16,8 +19,8 @@ public class Input {
 //          System.out.println(Arrays.toString(arr));
 //        // taking input using loop
 //        System.out.println("please enter the input of the array:");
-//        for (int i = 0 ; i< arr.length;i++){
-//            arr[i]= in.nextInt();
+//         for (int i = 0 ; i< arr.length;i++){
+////            arr[i]= in.nextInt();
 //        }
 
         // printing output without using loop
