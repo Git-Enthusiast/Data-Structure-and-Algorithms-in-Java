9a8e40d7ec74de07fdc47686bb68dab6e169ac35
Git-Enthusiast <messachuate@gmail.com>
2024-03-05 12:05:19 +0530

    Practice: Functions

---
+++ src/com/Practice/Functions/returnSum.java
@@ -0,0 +1,23 @@
+package com.Practice.Functions;
+
+/*
+Write a program to print the sum of two numbers
+entered by user by defining your own method.
+ */
+import java.util.Scanner;
+
+public class returnSum {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please Enter the Value of two Number:");
+        double num1 = input.nextDouble();
+        double num2 = input.nextDouble();
+        double sum = getSum(num1,num2);
+        System.out.println("The Sum of "+num1+" and "+num2+" is equal to : "+sum);
+
+    }
+
+    private static double getSum(double num1, double num2) {
+        return num1+num2;
+    }
+}
