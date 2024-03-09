9c2907453bbf75779dc17d83b895d65fa1bd3cb4
Git-Enthusiast <messachuate@gmail.com>
2024-03-09 22:15:37 +0530

    Practice:Pattern

---
+++ src/com/Practice/Pattern/Pattern_17.java
@@ -0,0 +1,12 @@
+package com.Practice.Pattern;
+
+import java.util.Scanner;
+
+public class Pattern_17 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please Enter the Value of n:a");
+        int n = input.nextInt();
+
+    }
+}
