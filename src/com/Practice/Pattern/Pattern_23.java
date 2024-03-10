3727b578a6e3235f8d85af4bccb93c2911692bc1
Git-Enthusiast <messachuate@gmail.com>
2024-03-10 11:37:20 +0530

    Practice:Pattern

---
+++ src/com/Practice/Pattern/Pattern_23.java
@@ -0,0 +1,30 @@
+package com.Practice.Pattern;
+
+/*
+Please Enter the Number of rows:
+5
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+
+ */
+import java.util.Scanner;
+
+public class Pattern_23 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please Enter the Number of rows:");
+        int n = input.nextInt();
+        for (int i =1;i<=n;i++){
+            for (int j =1;j<=n-i;j++){
+                System.out.print("  ");// Only Two Space
+            }
+            for (int k = 1;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
