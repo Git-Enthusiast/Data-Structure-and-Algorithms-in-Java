8c0c3f7ce02b0668d9907b6bf991644192d55cb9
Git-Enthusiast <messachuate@gmail.com>
2024-03-12 21:53:38 +0530

    Java Exam

---
+++ src/com/JavaExam/MyClass.java
@@ -0,0 +1,24 @@
+package com.JavaExam;
+
+public class MyClass {
+    // Method with default access level (package-private)
+    void defaultMethod() {
+        // Can be accessed within the same package
+    }
+
+    // Method with public access level
+    public void publicMethod() {
+        // Can be accessed from any other class
+    }
+
+    // Method with private access level
+    private void privateMethod() {
+        // Can be accessed only within this class
+    }
+
+    // Method with protected access level
+    protected void protectedMethod() {
+        // Can be accessed within the same package and by subclasses
+    }
+}
+
