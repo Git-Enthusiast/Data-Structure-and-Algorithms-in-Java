22de91359bbe08b6495afd34da941254ccc8587b
Git-Enthusiast <messachuate@gmail.com>
2024-03-13 10:02:23 +0530

    Practice:  Pattern_22 Updated

--- src/com/Practice/Pattern/Pattern_22.java
+++ src/com/Practice/Pattern/Pattern_22.java
@@ -16,9 +16,9 @@ public class Pattern_22 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Please Enter the Number of rows:");
-        int n = input.nextInt();
-        for (int i =1;i<=n;i++){
-            for (int j =1;j<=n-i;j++){
+        int numRows = input.nextInt();
+        for (int i =1;i<=numRows;i++){
+            for (int j =1;j<=numRows-i;j++){
                 System.out.print(" ");// Only One Space
             }
             for (int k = 1;k<=i;k++){
