9fe4d11ee7c8184ba02ec8bf68e4d0d59e8b2f19
Git-Enthusiast <messachuate@gmail.com>
2024-03-19 20:38:13 +0530

    Practice:  Pattern_24

--- src/com/Practice/Pattern/Pattern_24.java
+++ src/com/Practice/Pattern/Pattern_24.java
@@ -1,5 +1,15 @@
 package com.Practice.Pattern;
 
+/*
+Please Enter the no of rows:
+5
+* * * * *
+* * * *
+* * *
+* *
+*
+
+ */
 import java.util.Scanner;
 
 public class Pattern_24 {
@@ -7,14 +17,14 @@ public class Pattern_24 {
         Scanner input = new Scanner(System.in);
         System.out.println("Please Enter the no of rows:");
         int  n = input.nextInt();
-        for (int i = 1;i<=n;i++){
-            for (int j =1;j<=n-i-1;j++){
-                System.out.print("* ");
-            }
-            for (int k = 1;k<=i-1;k++){
-                System.out.print("  ");
-            }
-            System.out.println();
-        }
+         for (int i = 1;i<=n;i++){
+             for (int j = 1;j<=n-i+1;j++){
+                 System.out.print("* ");
+             }
+             for (int k = 1;k<=i-1;k++){
+                 System.out.print("  ");
+             }
+             System.out.println();
+         }
     }
 }
