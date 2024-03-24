4013d05f6ab397326d8798b8ccbf6c432d1fa7bc
Git-Enthusiast <messachuate@gmail.com>
2024-03-24 16:14:40 +0530

    Java_Core_Advance: Updated File - 4

---
+++ src/com/Java_Core_Advance/_4_Hello.java
@@ -0,0 +1,96 @@
+package com.Java_Core_Advance;
+
+/*
+You can use the JDK compiler to convert your Java text file into an executable program. Your Java text segment is
+converted into bytecode after compilation which carries the .class extension.
+
+Component
+Use
+
+javac
+
+ Java compiler converts source code into Java bytecode
+java
+
+ The loader of the java apps.
+javap
+
+ Class file disassembler,
+javadoc
+
+ Documentation generator,
+jar
+
+ Java Archiver helps manage JAR files.
+appletviewer
+
+  Debugging of Java applets without a web browser,
+xjc
+
+  Accepts an XML schema and generates Java classes,
+apt
+
+  Annotation-processing tool,
+jdb
+
+  Debugger,
+jmc
+
+  Java Mission Control,
+JConsole
+
+  Monitoring and Management Console,
+pack200
+
+  JAR compression tool,
+extcheck
+
+  Utility tool to detects JAR file conflicts,
+idlj
+
+  IDL-to-Java compiler,
+keytool
+
+  The keystore manipulating tool,
+jstatd
+
+  jstat daemon (experimental)
+jstat
+
+  JVM statistics monitoring tool
+jshell
+
+  jshell introduced in java 9.
+jstack
+
+ Prints Java stack traces(experimental)
+jrunscript
+
+  Java command-line script shell.
+jhat
+
+ Java Heap Analysis Tool (experimental)
+jpackage
+
+ Generate self-contained application bundles.
+javaws
+
+ Web Start launcher for JNLP applications,
+javah
+
+ C header and stub generator,
+jarsigner
+
+ jar signing and verification tool
+jinfo
+
+configuration information(experimental)
+javafxpackager
+
+Package and sign JavaFX applications
+ */
+public class _4_Hello {
+    public static void main (String[] args) {
+        System.out.println("Hello Geek!");
+    }
+}
