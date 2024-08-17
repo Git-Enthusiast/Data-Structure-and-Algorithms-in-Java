5f986e0913580c8cf009976177d92d17863ff46b
Git-Enthusiast <messachuate@gmail.com>
2024-08-17 20:35:52 +0530

    Java : Lec 30 OOPs class 1

---
+++ src/com/Lec30/Main.java
@@ -0,0 +1,29 @@
+package com.Lec30;
+
+public class Main {
+    public static void main(String[] args) {
+        // Store 5 roll nos
+        int[] numbers = new int[5];
+
+        // store marks of 5 students
+        float[] marks = new float[5];
+
+        // store Names of 5 Students
+        String[] names = new String[5];
+
+        // data of 5 Students:{ Roll no, Name, Marks}
+        int[] rno = new int[5];
+        String[] name = new String[5];
+        float[] mark = new float[5];
+
+        Student[] students = new Student[5];
+        Student rajan;
+    }
+    // create a class
+    class Student {
+        int[] rno = new int[5];
+        String[] name = new String[5];
+        float[] mark = new float[5];
+
+    }
+}
