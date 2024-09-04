e7db11764560885c2e80091d830c7889c9dc6478
Git-Enthusiast <messachuate@gmail.com>
2024-09-05 00:11:07 +0530

    Java : LeetCode :goodPairs Added.

---
+++ src/com/LeetCode/Arrays/goodPairs.java
@@ -0,0 +1,48 @@
+package com.LeetCode.Arrays;
+/*
+1512. Number of Good Pairs
+Easy
+Topics
+Companies
+Hint
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+Example 2:
+
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+
+
+Constraints:
+
+1 <= nums.length <= 100
+1 <= nums[i] <= 100
+ */
+public class goodPairs {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1,1,3};
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[i] == nums[j]){
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
