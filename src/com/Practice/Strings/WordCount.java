41e7f171c7e31cb60fcd4f0ba2ff5eb369e61e5b
Git-Enthusiast <messachuate@gmail.com>
2024-09-09 13:12:46 +0530

    Java : Practice : word count: Modified.

--- src/com/Practice/Strings/WordCount.java
+++ src/com/Practice/Strings/WordCount.java
@@ -4,7 +4,6 @@ public class WordCount {
     public static void main(String[] args) {
         String input = "I am a java developer";
         int wordCount = countWords(input);
-
         System.out.println("Original String: " + input);
         System.out.println("Total Number of Words: " + wordCount);
     }
@@ -12,7 +11,6 @@ public class WordCount {
     public static int countWords(String input) {
 
         String[] words = input.split("\\s+");
-
         return words.length;
     }
 }
