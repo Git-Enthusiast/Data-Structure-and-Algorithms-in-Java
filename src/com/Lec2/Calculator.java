7e730929ab6c9a76660813873042215fcb366a68
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2025-09-08 13:35:11 +0530

    Refactor Calculator class to use try-with-resources for Scanner initialization and improve input handling

--- src/com/Lec2/Calculator.java
+++ src/com/Lec2/Calculator.java
@@ -4,41 +4,40 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        // Take input from user until user press 'X' or 'x'.
-        int ans = 0;
-        while (true){
-            System.out.println("Please Enter the operator:");
-            char op = in.next().trim().charAt(0);
-            if (op=='+'||op=='-'||op=='%'||op=='*'||op=='/'){
-                System.out.println("Please Enter value of two Number:");
-                int num1 = in.nextInt();
-                int num2 = in.nextInt();
-                if (op == '+') {
-                    ans = num1+num2;
-                }
-                if (op == '-'){
-                    ans = num1-num2;
-                }
-                if (op == '*'){
-                    ans = num1*num2;
-                }
-                if (op == '%'){
-                    ans = num1%num2;
-                }
-                if (op == '/'){
-                    if (num2>0){
-                        ans = num1/num2;
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Take input from user until user press 'X' or 'x'.");
+            int ans = 0;
+            while (true) {
+                System.out.println("Please Enter the operator:");
+                char op = in.next().trim().charAt(0);
+                if (op == '+' || op == '-' || op == '%' || op == '*' || op == '/') {
+                    System.out.println("Please Enter value of two Number:");
+                    int num1 = in.nextInt();
+                    int num2 = in.nextInt();
+                    if (op == '+') {
+                        ans = num1 + num2;
                     }
+                    if (op == '-') {
+                        ans = num1 - num2;
+                    }
+                    if (op == '*') {
+                        ans = num1 * num2;
+                    }
+                    if (op == '%') {
+                        ans = num1 % num2;
+                    }
+                    if (op == '/') {
+                        if (num2 > 0) {
+                            ans = num1 / num2;
+                        }
+                    }
+                } else if (op == 'X' || op == 'x') {
+                    break;
+                } else {
+                    System.out.println("invalid Input");
                 }
+                System.out.println(ans);
             }
-            else if (op == 'X'||op=='x') {
-                break;
-            }
-            else {
-                System.out.println("invalid Input");
-            }
-            System.out.println(ans);
         }
     }
 }
