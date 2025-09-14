64fb924aee5281f141be7d70672aea683a940816
Rajan Raj <141961731+Git-Enthusiast@users.noreply.github.com>
2025-09-14 10:12:33 +0530

    Refactor Greeting class to utilize try-with-resources for Scanner initialization and improve code readability

--- src/com/Lec4/Greeting.java
+++ src/com/Lec4/Greeting.java
@@ -5,18 +5,22 @@ import java.util.Scanner;
 public class Greeting {
     public static void main(String[] args) {
         greeting();
-        Scanner in = new Scanner(System.in);
-        System.out.println("Please Enter your name :");
-        String name = in.nextLine();
-        String message = getGreet(name);
-        System.out.println(message);
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Please Enter your name :");
+            String name = in.nextLine();
+            String message = getGreet(name);
+            System.out.println(message);
+        } catch (Exception e) {
+            System.out.println("Exception handled");
+        }
+
     }
 
     private static String getGreet(String name) {
-        return "Hey, "+name+" How are you ?";
+        return "Hey, " + name + " How are you ?";
     }
 
-    static void greeting(){
+    static void greeting() {
         System.out.println("Hello World!");
     }
 }
